37eebf5f32a62ee698c5ed797a8e74ef775661ca
lucas.singh <lucas.singh@bizitglobal.com>
2023-06-27 16:50:09 -0300

    fix marginsss sec

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -6,10 +6,10 @@ import '../styles/about.scss';
 const About = () => {
     return (
         <section className='section-about'>
-            <div className='container pb-5'>
+            <div className='container pt-5 pb-5'>
                 <Row>
                     <Col md={6} className='d-md-flex justify-content-md-center align-items-md-start flex-md-column'>
-                        <h2 className='title-about- mb-3'>Que es un Arborista?</h2>
+                        <h2 className='title-about- mb-3 mt-3'>Que es un Arborista?</h2>
                         <span className='text'>Un Arborista es quien, con intención, cuida de
                             un árbol. Es quien lo hace florecer y lo ayuda a
                             sanar cuando enferma. Es quien alimenta la tierra,
